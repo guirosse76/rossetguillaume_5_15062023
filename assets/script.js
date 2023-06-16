f3f3d0bc83f8f1b08df8e6396a2d212321ebef9a
Guillaume <rossetguillaume759@gmail.com>
2023-06-16 16:40:10 +0200

    etape 1 et 2 du projet

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,73 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// On récupère les deux flèches
+let next = document.querySelector(".arrow_right")
+let prev = document.querySelector(".arrow_left")
+
+// On met en place les écouteurs d'évènements sur les flèches
+next.addEventListener("click", slideNext)
+prev.addEventListener("click", slidePrev)
+
+let nombres = slides.length
+const dot = document.getElementById('.dots')
+
+function slideNext(){
+	console.log(next)
+}
+
+function slidePrev(){
+	console.log(prev)
+}
+
+// // On récupère le conteneur principal du diaporama
+// const diapo = document.querySelector("#banner")
+
+// // On récupère le conteneur de tous les éléments
+// elements = document.querySelector("banner-img")
+
+// // On récupère un tableau contenant la liste des diapos
+// // slides = Array.from(elements.children)
+
+
+
+// // Variables globales
+// let compteur = 0 // Compteur qui permettra de savoir sur quelle slide nous sommes
+// let timer, elements, slideWidth
+
+
+
+// /**
+//  * Cette fonction fait défiler le diaporama vers la droite
+//  */
+// function slideNext(){
+//     // On incrémente le compteur
+//     compteur++
+
+//     // Si on dépasse la fin du diaporama, on "rembobine"
+//     if(compteur == slides.length){
+//         compteur = 0
+//     }
+
+//     // On calcule la valeur du décalage
+//     let decal = -slideWidth * compteur
+//     elements.style.transform = `translateX(${decal}px)`
+// }
+
+// /**
+//  * Cette fonction fait défiler le diaporama vers la gauche
+//  */
+// function slidePrev(){
+//     // On décrémente le compteur
+//     compteur--
+
+//     // Si on dépasse le début du diaporama, on repart à la fin
+//     if(compteur < 0){
+//         compteur = slides.length - 1
+//     }
+
+//     // On calcule la valeur du décalage
+//     let decal = -slideWidth * compteur
+//     elements.style.transform = `translateX(${decal}px)`
+// }
